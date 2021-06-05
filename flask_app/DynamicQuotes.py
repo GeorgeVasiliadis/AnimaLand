1145e12c7fce7ee87dfdf3a4e761ab391f1b660a
George Vasiliadis <geor.vasiliadis@gmail.com>
2021-06-06 01:14:11 +0300

    Intergrated quote storage as .json file
    Closes #10

--- flask_app/DynamicQuotes.py
+++ flask_app/DynamicQuotes.py
@@ -1,21 +1,18 @@
 import random
+import json
 
 # General format of pack: Tuple of quote and author - ("quote", "author")
 # Quotes is a list of packs
 
-quotes = [
-("Love the Earth as you would love yourself.", None),
-("Be a part of the solution, not a part of the pollution.", None),
-("The Earth does not belong to man. Man belongs to the Earth!", None),
-("The Earth is what we all have in common.", "Wendell Berry"),
-("Nature is painting for us, day after day, pictures of infinite beauty.", "Jon Ruskin"),
-("The Earth is a fine place and worth fighting for.", "Ernest Hemingway"),
-("What I stand for, is what I stand on.", "Wendell Berry"),
-("The world owes you nothing. It was here first.", "Mark Twain"),
-("If you love the Earth, it will love you back.", None),
-("Make the planet great again.", None),
-("The Best Way To Get Started Is To Quit Talking And Begin Doing.", "Walt Disney")
-]
+#
+quotes = []
+
+# Try to load quotes from file. If that fails, initialize quotes to a simple quote.
+try:
+    with open("quotes.json") as f_in:
+        quotes = json.load(f_in)
+except:
+    quotes = [["The Earth is a fine place and worth fighting for.", "Ernest Hemingway"]]
 
 def randomQuote():
     pack = random.choice(quotes)
