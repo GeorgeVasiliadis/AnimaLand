bfa995ed0a81fb4c810929abc5fe577d2d933d29
George Vasiliadis <geor.vasiliadis@gmail.com>
2021-06-05 16:11:38 +0300

    Implemented feature as requested by #12

--- flask_app/authSession.py
+++ flask_app/authSession.py
@@ -1,5 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, flash
-from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Blueprint, render_template, redirect, url_for, request
 from flask_login import login_user, logout_user, login_required
 from .models import User
 from . import db
@@ -19,9 +18,16 @@ def register_post():
     db.session.add(user)
     db.session.commit()
 
-    return redirect(url_for("authBlueprint.show"))
+    return redirect(url_for("authBlueprint.manage_accounts"))
 
-@authBlueprint.route("/show.html")
-def show():
+@authBlueprint.route("/manage-accounts.html")
+def manage_accounts():
     users = User.query.all()
-    return render_template("show.html", users=users)
+    return render_template("manage-accounts.html", users=users)
+
+@authBlueprint.route("/manage/delete/account/<int:id>")
+def delete_account(id):
+    user = User.query.get_or_404(id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for("authBlueprint.manage_accounts"))
--- flask_app/models.py
+++ flask_app/models.py
@@ -5,8 +5,8 @@ from . import db
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(100), unique=True, nullable=False)
-    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(80), nullable=False)
 
 #for the petitions
 class Petition(db.Model):
