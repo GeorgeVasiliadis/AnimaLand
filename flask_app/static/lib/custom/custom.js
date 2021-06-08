dbec4ba73710b6ae338bfce651dba16a2e5637ab
Zoe Kelepiri <56219829+zoekelepiri@users.noreply.github.com>
2021-06-08 11:50:07 +0300

    Update custom.js

--- flask_app/static/lib/custom/custom.js
+++ flask_app/static/lib/custom/custom.js
@@ -19,4 +19,14 @@
       toTop.classList.remove("active");
     }
   })
+  
+  function onChange() {
+    const password = document.querySelector('input[name=password]');
+    const confirm = document.querySelector('input[name=password2]');
+    if (confirm.value === password.value) {
+      confirm.setCustomValidity('');
+    } else {
+      confirm.setCustomValidity('Passwords do not match');
+    }
+  }
   }
