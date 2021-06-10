0a0aeae9826316f621e908acfe0616e56e7afe9c
George Vasiliadis <geor.vasiliadis@gmail.com>
2021-06-10 16:07:41 +0300

    SearchEngine.py - Added comments. Minor code clean-up. More pythonic code

--- flask_app/SearchEngine.py
+++ flask_app/SearchEngine.py
@@ -1,33 +1,48 @@
+"""
+Module used to provide a searching engine on top of Petition objects.
+"""
+
 from . import db
 from .models import Petition
 
 def scorePetition(petition, *keywords):
+    """Examine the given petition and return its relevance score againts the
+    given keywords."""
+
     score = 0
+
     for key in keywords:
+
+        # Normalize both key and strings to be searched
         key = key.lower()
         title = petition.title.lower()
         content = petition.content.lower()
 
+        # Score eache part of the Petition depending on the number of occurancies
         titleScore = title.count(key)
         contentScore = content.count(key)
+
+        # Give more weight to the keys found in title
         score += titleScore*50 + contentScore
+
     return score
 
 def search(*keywords):
 
-    # Fetch
+    # Fetch Petitions
     petitions = Petition.query.all()
 
-    # Score
-    relevanceList = []
-    for petition in petitions:
-        score = scorePetition(petition, *keywords)
-        relevanceList.append((score, petition))
+    # For each Petition, append its score along with itself into the
+    # relevanceList
+    relevanceList = [
+        (scorePetition(petition, *keywords), petition)
+        for petition in petitions
+    ]
 
-    # Sort: put more relevant first
+    # Sort: place more relevant (high-scored) petitinos first
     relevanceList.sort(key=lambda struct: struct[0], reverse=True)
 
-    # Clean: remove zero-occurancies
+    # Clean: remove irrelevant (zero-occurancies)
     relevanceList = [x[1] for x in relevanceList if x[0] > 0]
 
     return relevanceList
