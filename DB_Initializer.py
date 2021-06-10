bf859085c834a42319a942456aa60f50ab40393c
George Vasiliadis <geor.vasiliadis@gmail.com>
2021-06-10 16:43:47 +0300

    DB_Initializer.py - Added comments. Minor code clean-up.

--- DB_Initializer.py
+++ DB_Initializer.py
@@ -1,3 +1,10 @@
+"""
+Database utility module, intended to be used by the "admin" of this web-app.
+
+It is also an important script for the developers as it automates most of the
+database manual setup.
+"""
+
 import os
 
 from flask_app import create_app, db
@@ -5,6 +12,10 @@ from flask_app import create_app, db
 DB_PATH = os.path.normpath("flask_app/db.sqlite" )
 
 def install():
+    """Create a new one database at `DB_PATH` only if there is no other database
+    existing there.
+    """
+
     if not os.path.exists(DB_PATH):
         db.create_all(app=create_app())
         print(f"Database was installed successfully: {DB_PATH}")
@@ -12,9 +23,17 @@ def install():
 
     print(f"Database couldn't be installed. There is already another \
 database in use: {DB_PATH}")
+
     return False
 
 def uninstall():
+    """Make sure that the database specified by `DB_PATH` will not exist after
+    this operation.
+
+    Succeeds even if the specified database couldn't be found.
+    Fails if there is some file IO error.
+    """
+
     if os.path.exists(DB_PATH):
         try:
             os.remove(DB_PATH)
@@ -29,6 +48,8 @@ def uninstall():
     return True
 
 def populateUsers():
+    """Create the default users and populate database."""
+
     from flask_app.models import User
 
     users = [
@@ -38,6 +59,8 @@ def populateUsers():
         User(username="Zoe", email="zkelepiri@csd.auth.gr", password="Zoe"),
         User(username="George", email="vgiorgos@csd.auth.gr", password="George")
     ]
+
+    # Add users to database
     for user in users:
         db.session.add(user)
 
@@ -45,6 +68,14 @@ def populateUsers():
     print("Users were populated successfully!")
 
 def populatePetitions():
+    """
+    Create the default petitions, populate database and "upload" the dependant
+    images.
+
+    This function depends strongly on `dummy` directory which co-exists in same
+    directory
+    """
+
     from shutil import copy2
     from flask_app.models import Petition
 
@@ -80,6 +111,8 @@ def populatePetitions():
             imagePath="iceberg.jpg"
         )
     ]
+
+    # Add petitions to database
     for petition in petitions:
         db.session.add(petition)
 
@@ -89,7 +122,10 @@ def populatePetitions():
     # Copy dummy images into appropriate location in flask_app
     prefix_old = os.path.normpath("dummy/")
     prefix_new = os.path.normpath("flask_app/static/uploads/res/")
+
+    # Ensure that the goal directory exists
     os.makedirs(prefix_new, exist_ok=True)
+
     for petition in petitions:
         old = os.path.join(prefix_old, petition.imagePath)
         new = os.path.join(prefix_new, petition.imagePath)
@@ -98,12 +134,22 @@ def populatePetitions():
     print("Thumbnail images for petitions were copied successfully!")
 
 def reset():
+    """
+    Uninstall (if exists) and reinstall the database. Then populate it with the
+    default records.
+
+    This function is especially useful for testing purposes when updating the
+    database schema.
+    """
+
     if not uninstall():
         print("Aborting...")
         return
+
     if not install():
         print("Aborting...")
         return
+        
     populateUsers()
     populatePetitions()
 
